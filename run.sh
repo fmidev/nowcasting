54bb8a1eddf334c94d9da968a8edfd6cee13bd21
Jussi Ylhäisi <jussi.ylhaisi@gmail.com>
2018-06-29 09:16:43 +0300

    the retrievals have a the same resolution and are in 'verif' mode: 6 hour history for both obs and pal, so sensitivity analysis can be done based on verif metrics

--- run.sh
+++ run.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 # This script downloads gridded data from Smartmet Server (test) for nowcasting purposes. Current state is coming from $USED_OBS (LAPS) and forecasts are from $USED_MODEL (
-# The script needs a trigger so that it is run whenever the latest LAPS analysis is ready. Currently it is possible that rounded to previous even hour there's no LAPS analysis available. LAPS analysis gets ready around 20 past?
+# The operational mode of the script needs a trigger so that it is run whenever the latest LAPS analysis is ready. Currently it is possible that rounded to previous even hour there's no LAPS analysis available. LAPS analysis gets ready around 20 past?
+# This script runs under "verification" mode so that it retrieves data from the past 6 hours from both pal_skandinavia and laps_skandinavia. It uses this data to both interpolate between timesteps and verification. 
+
 
 # Known issues:
 # SOLVED predictability (the max time length over which persistence is applied to) as an input parameter is not applied at the moment
-# is there need to have predictability defined in time-span where model data is not available? If this was done, model data would also need to be interpolated from 0 and 3 hour forecasts...
-# no error checking or triggering for the LAPS analysis of current hour whether it is available from Smartmet Server or not
-# Data coming from Smartmet Server is not yet reprojected to EPSG 3067 (or whatever projection is defined in $CONFPATH$CONFFILE$PROJ)
 # SOLVED The bbox of DOMAIN SCAND2 is burned to wget retrievals atm. These bbox boundaries would need to be actually loaded in from conf files, which are specified in directory /fmi/dev/run/radar/conf/conf/
-# For precipitation, radar data RATE composites and model data instantaneous RATES would be needed. These would need to have a somewhat comparable temporal resolution.
+# SOLVED laps_skandinavia -grid needs to have similar resolution than edited data (pal_skandinavia). The grid definition needs to be retrieved from the modeldata.nc file
+# SOLVED Smartmet server -retrievals currently contain several timesteps...restrict to one analysis and forecast length (defined by predictability). This is fixed just by including timesteps=$PREDICTABILITY to the retrievals.
+# NECESSARYNo instantaneous precipitation in pal_skandinavia?
+# NECESSARY learn how to read in netcdf files to Python
+# NECESSARY for the function interpolation.advection, variables quantity_min/quantity_max need to be defined specificly for each variable.
+# NECESSARY Farneback parameters: For what are these used? If for optical flow algorithm, what is the sensitivity of the forecasts to them (winsize in particular)?
+## What verification metrics would need to be calculated from the sensitivity experiments?
+## For practically any verif metrics (like field deformation or similar), previous merged forecasts need to be compared against current analysis. For development this is much much easier if analysis+forecasts are selected to be in the past so that verifying analyses are always available. Verification metrics can be calculated for both motion vectors calculated either forward and backward in time.
+# NECESSARY Visualization of the results? Through Turso-page and R scripts?
+# NECESSARY FOR OPERATIVE VERSION No error checking or triggering for the LAPS analysis of current hour whether it is available from Smartmet Server or not
+
+# RADAR DATA CONVERSION TO EPSG 4326 IS MAYBE MORE PREFERABLE? Data coming from Smartmet Server is not yet reprojected to EPSG 3067 (or whatever projection is defined in $CONFPATH$CONFFILE$PROJ)
+# NOT ATM is there any need to have predictability defined in time-span where model data is not available? If this was done, model data would also need to be interpolated from 0 and 3 hour forecasts...Also, in the script call_interpolation.py the variable "n_interp_frames" would need to be accordingly defined as an even number.
+# NEXT STEP For precipitation, radar data RATE composites and model data instantaneous RATES would be needed. These would need to have a somewhat comparable temporal resolution.
+# QUESTION why filtering R1_f and R2_f in interpolation.py?
 #
 ####### Input parameters ########
 # (remember to call shell scripts with named arguments like DATAPATH="/fmi/somepath/" ./run.sh though it is not necessary as there are always default values specified!) 
 
 ####### General parameters
+PYTHON=${PYTHON:-'/fmi/dev/python_virtualenvs/venv/bin/python'}
 USED_OBS=${USED_OBS:-"laps_skandinavia"}
 USED_MODEL=${USED_MODEL:-"pal_skandinavia"}
 PREDICTABILITY=${PREDICTABILITY:-"3"} # This must be an even number, as model data or production system operates on max one hour temporal resolution.
 SECONDS_BETWEEN_STEPS=${SECONDS_BETWEEN_STEPS:-3600} # One hour resolution is required for production. For illustrative purposes of the algorithm, even higher resolution can be used
-PYTHON=${PYTHON:-'/fmi/dev/python_virtualenvs/venv/bin/python'}
 DOMAIN=${DOMAIN:-"SCAND2"} # this specifies the extent of the domain which is used (the name of the conf file)
 CONFDIR=${CONFDIR:-"/fmi/dev/run/radar/conf/conf/"} # All the domain configurations are located here
 DATAPATH=${DATAPATH:-"/fmi/data/nowcasting/"}
 OUTPATH=${OUTPATH:-"/fmi/$FMI_RUN_ENV/products/cache/nowcasting/"} # Nowcasting fields are output to this directory
-OUTFILE_INTERP=${OUTFILE_INTERP:-'{}_${DOMAIN}.h5'}
-OUTFILE_SUM=${OUTFILE_SUM:-'{}_5minsum_${DOMAIN}.h5'}
+OUTFILE_INTERP=${OUTFILE_INTERP:-'{}_${DOMAIN}.nc'}
+OUTFILE_SUM=${OUTFILE_SUM:-'{}_5minsum_${DOMAIN}.nc'}
 echo $DATAPATH
 echo $OUTPATH
 if [ ! -d $DATAPATH ]; then
@@ -40,10 +53,10 @@ fi
 ####### Time stamps. These are rounded to previous hour.
 timestamp_now=`eval date -u -d "now" +"%Y%m%d%H"`00
 timestamp_fcst=$(eval 'date -u -d "now + $PREDICTABILITY hour" +"%Y%m%d%H"')00
-timestamp_m4hours=$(eval 'date -u -d "now - 4 hour" +"%Y%m%d%H"'00)
+timestamp_m6hours=$(eval 'date -u -d "now - 6 hour" +"%Y%m%d%H"'00)
 echo $timestamp_now
 echo $timestamp_fcst
-echo $timestamp_m4hours
+echo $timestamp_m6hours
 YEAR=${timestamp_now:0:4}
 MONTH=${timestamp_now:4:2}
 DAY=${timestamp_now:6:2}
@@ -63,11 +76,11 @@ COMPOSITE=$COMPOSITE_DIR/$COMPOSITE_BASENAME
 ####### Defining actual boundary area
 # Read in conf file
 query="source "$CONFDIR$DOMAIN".cnf"
-echo $query
+# echo $query
 eval $query
-echo $BBOX
-echo $PROJ
-echo $SIZE
+# echo $BBOX
+# echo $PROJ
+# echo $SIZE
 
 
 
@@ -85,12 +98,12 @@ echo $SIZE
 
 
 #for parameters in Pressure, GeopHeight, Temperature, DewPoint, Humidity, Visibility, PressureAtStationLevel, WindSpeedMS, WindDirection, WindVectorMS, HourlyMaximumGust, WindUMS, WindVMS, SurfaceWaterPhase #these parameters are available for the producer laps_skandinavia
-for parameter in Pressure Temperature
+for PARAMETER in Pressure Temperature
 do
 
 
-OBSDATA="$timestamp_now"_"$USED_OBS"_DOMAIN="$DOMAIN"_"$parameter".nc
-MODELDATA="$timestamp_now"_fcst"$timestamp_fcst"_"$USED_MODEL"_DOMAIN="$DOMAIN"_"$parameter".nc
+OBSDATA="$timestamp_now"_"$USED_OBS"_DOMAIN="$DOMAIN"_"$PARAMETER".nc
+MODELDATA="$timestamp_now"_fcst"$timestamp_fcst"_"$USED_MODEL"_DOMAIN="$DOMAIN"_"$PARAMETER".nc
 echo $OBSDATA
 echo $MODELDATA
 
@@ -99,25 +112,40 @@ echo $MODELDATA
 # Working retrieval to ECMWF-data generated by Marko
 #wget -O out2.nc --no-proxy 'smartmet.fmi.fi/download?param=Temperature&producer=ecmwf_eurooppa_pinta&format=netcdf&bbox=19.1,59.7,31.7,70.1&timesteps=24&projection=epsg:4326'
 
+# Retrieving pal forecast over Scandinavian domain
+query="wget -O "$DATAPATH$MODELDATA" --no-proxy 'http://smartmet.fmi.fi/download?param="$PARAMETER"&producer="$USED_MODEL"&format=netcdf&starttime="$timestamp_m6hours"&endtime="$timestamp_now"&bbox="$BBOX"&projection=epsg:4326'"
+echo $query
+eval $query
+# Change to netcdf4 type file OR DO THE REPROJECTION HERE AND READ IN NETCDF3 FILES IN PYTHON SCRIPT
+query="nccopy -k 4 "$DATAPATH$MODELDATA" "$DATAPATH"modeldata.nc"
+echo $query
+eval $query
+# Retrieve grid information from edited data (modeldata.nc file) and use this to retrieve a grid of same size for laps_skandinavia
+query='ncdump -h '$DATAPATH'modeldata.nc | grep "lat ="'
+echo $query
+SIZE_LAT=$(eval $query)
+SIZE_LAT=`echo ${SIZE_LAT:7} | rev`
+SIZE_LAT=`echo ${SIZE_LAT:2} | rev`
+query='ncdump -h '$DATAPATH'modeldata.nc | grep "lon ="'
+echo $query
+SIZE_LON=$(eval $query)
+SIZE_LON=`echo ${SIZE_LON:7} | rev`
+SIZE_LON=`echo ${SIZE_LON:2} | rev`
+echo $SIZE_LAT
+echo $SIZE_LON
 
 # Retrieving LAPS data over Scandinavian domain
-query="wget -O "$DATAPATH$OBSDATA" --no-proxy 'http://smartmet.fmi.fi/download?param="$parameter"&producer="$USED_OBS"&format=netcdf&origintime="$timestamp_now"&bbox="$BBOX"&projection=epsg:4326'"
+query="wget -O "$DATAPATH$OBSDATA" --no-proxy 'http://smartmet.fmi.fi/download?param="$PARAMETER"&producer="$USED_OBS"&format=netcdf&gridsize="$SIZE_LON","$SIZE_LAT"&starttime="$timestamp_m6hours"&endtime="$timestamp_now"&bbox="$BBOX"&projection=epsg:4326'"
+echo $query
+eval $query
+# IF NO LAPS AVAILABLE IN OPERATIVE MODE THEN WHAT?! ADD ERROR CHECKING HERE!
+# Change to netcdf4 type file OR DO THE REPROJECTION HERE AND READ IN NETCDF3 FILES IN PYTHON SCRIPT
+query="nccopy -k 4 "$DATAPATH$OBSDATA" "$DATAPATH"obsdata.nc"
 echo $query
 eval $query
 
-# IF NO LAPS AVAILABLE THEN WHAT?! ADD ERROR CHECKING HERE!
 
- 
-
-# Retrieving pal forecast over Scandinavian domain
-query="wget -O "$DATAPATH$MODELDATA" --no-proxy 'http://smartmet.fmi.fi/download?param="$parameter"&producer="$USED_MODEL"&format=netcdf&starttime="$timestamp_fcst"&bbox="$BBOX"&projection=epsg:4326'"
-echo $query
-eval $query
-
-#wget -O latest_pal.nc --no-proxy 'smartmet.fmi.fi/download?param=Temperature&producer=pal_skandinavia&format=netcdf&bbox=4.00118791,50.00007491,42.99146951,72.99231333&timesteps=3&projection=epsg:4326'
-
-
-
+# FOR THE PROJECTION CONVERSIONS
 # this is not working really
 # gdalwarp -t_srs epsg:3067 -of netCDF 201805311000_fcst201805311300_pal_skandinavia_DOMAIN\=TULISET2_Pressure.nc output.nc
 # This only changes the data type from .nc to HDF5
@@ -125,13 +153,6 @@ eval $query
 # see also https://www.unidata.ucar.edu/software/netcdf/docs/interoperability_hdf5.html
 # probably it is sufficient to only make projection changes and not turn Netcdf file into HDF5. nc4 files can be read with HDF5 programs and vice versa.
 
-# Change to netcdf4 type file OR DO THE REPROJECTION HERE AND READ IN NETCDF3 FILES IN PYTHON SCRIPT
-query="nccopy -k 4 "$DATAPATH$OBSDATA" "$DATAPATH"obsdata.nc"
-echo $query
-eval $query
-query="nccopy -k 4 "$DATAPATH$MODELDATA" "$DATAPATH"modeldata.nc"
-echo $query
-eval $query
 
 
 
@@ -140,10 +161,15 @@ eval $query
 
 # END
 
-# Similar call as for generate.sh USE THIS
+# Updated call with additional parameters
+#cmd=$PYTHON" call_interpolation_testi.py --first_field "$DATAPATH"obsdata.nc --second_precip_field "$DATAPATH"obsdata.nc --seconds_between_steps "$SECONDS_BETWEEN_STEPS" --output_interpolate "$OUTPATH$OUTFILE_INTERP" --predictability "$PREDICTABILITY" --parameter "$PARAMETER
+
+# Similar call and parameters as for generate.sh in /fmi/dev/run/radar/amv/spoflow/interpolate/precipfields USE THIS!!
 #cmd="$PYTHON call_interpolation.py --first_precip_field $DATAPATH/$OBSDATA_reprojected.nc --second_precip_field $DATAPATH/$MODELDATA_reprojected.nc --seconds_between_steps $SECONDS_BETWEEN_STEPS --output_interpolate $OUTPATH/$OUTFILE_INTERP --output_sum $OUTPATH/$OUTFILE_SUM"
-#echo $cmd
-#eval $cmd
+# THIS WORKS!
+# cmd="$PYTHON call_interpolation_testi.py --first_precip_field /fmi/data/nowcasting/testdata_radar/opera_rate/T_PAAH21_C_EUOC_20180613120000.hdf --second_precip_field /fmi/data/nowcasting/testdata_radar/opera_rate/T_PAAH21_C_EUOC_20180613121500.hdf --seconds_between_steps 30 --output_interpolate $OUTPATH/$OUTFILE_INTERP --output_sum $OUTPATH/$OUTFILE_SUM"
+echo $cmd
+eval $cmd
 
 ##  Convert output to png?
 # OUTFILES=`echo $OUTFILE | awk -F{} '{print $2}'`
